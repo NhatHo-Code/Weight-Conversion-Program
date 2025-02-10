2fd51e5ef62905e1011468d5bf0db1f8a8eafb1a
NhatHo-Code <honguyennminhnhat06@gmail.com>
2025-02-10 22:08:00 +0700

    Initial commit: Added weight conversion program

---
+++ src/Main.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String [] args){
+        Scanner scnr = new Scanner(System.in);
+
+        // WEIGHT CONVERSION PROGRAM
+
+        double userWeight;
+        int userInput;
+        double converted;
+
+        System.out.println("Welcome to the weight conversion program");
+        System.out.println("Please type in your weight: ");
+        userWeight = scnr.nextDouble();
+
+        System.out.println("Please choose between converting from lbs to kg or kg to lbs");
+        System.out.println("For lbs to kg type 1, for kg to lbs type 2");
+        userInput = scnr.nextInt();
+
+        if(userInput == 1){
+            converted = userWeight / 2.205;
+            System.out.println("Your weight is " + converted + "kg");
+
+        }
+        else if(userInput == 2){
+            converted = userWeight * 2.205;
+            System.out.println("Your weight is " + converted + "lbs");
+
+        }
+
+        while(userInput != 1 && userInput != 2){
+            System.out.println("Not a valid choice");
+            System.out.print("Please enter a valid choice: ");
+            userInput = scnr.nextInt();
+
+            if(userInput == 1){
+                converted = userWeight / 2.205;
+                System.out.println("Your weight is " + converted + "kg");
+
+            }
+            else if(userInput == 2){
+                converted = userWeight * 2.205;
+                System.out.println("Your weight is " + converted + "lbs");
+
+            }
+        }
+        scnr.close();
+    }
+}
